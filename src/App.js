9c431cae1d6a61f337d7a6746e7fbb25f90477d0
deepa <deepagt445@gmail.com>
2025-05-30 01:17:06 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,33 +1,28 @@
 import "./App.css";
 import Home from "./Pages/Home.tsx";
 import Navbar from "./components/Navbar.tsx";
-import { BrowserRouter, Routes ,Route} from "react-router-dom";
-
- import MovieList from "./Pages/MovieList.tsx";
- import MovieDetails from "./Pages/MovieDetails.tsx";
- import Toprated from "./Pages/Toprated.tsx";
+import { HashRouter, Routes, Route } from "react-router-dom";
+import MovieList from "./Pages/MovieList.tsx";
+import MovieDetails from "./Pages/MovieDetails.tsx";
+import Toprated from "./Pages/Toprated.tsx";
 import Actordetails from "./Pages/ActorDetails.tsx";
 import ComingSoon from "./Pages/ComingSoon.tsx";
+
 function App() {
   return (
-   <BrowserRouter>
+    <HashRouter>
       <div className="min-h-screen bg-black text-white">
         <Navbar />
         <Routes>
-        
           <Route path="/" element={<Home/>}/>
           <Route path="/movies" element={<MovieList/>}/>
           <Route path="/movie/:id" element={<MovieDetails/>}/>
           <Route path="/actor/:id" element={<Actordetails/>}/>
           <Route path="/top-rated" element={<Toprated/>}/>
-       
           <Route path="/comingsoon" element={<ComingSoon />} />
-          
-          
         </Routes>
-        </div>
-      </BrowserRouter>
-   
+      </div>
+    </HashRouter>
   );
 }
 
